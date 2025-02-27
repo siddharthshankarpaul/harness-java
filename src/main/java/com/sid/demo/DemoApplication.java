c081d1d6dbf2f93dea2363618201a357c2f4d2ae
siddharth Paul <siddharth.paul@zemosolabs.com>
2025-02-27 18:50:49 +0530

    add failure

--- src/main/java/com/sid/demo/DemoApplication.java
+++ src/main/java/com/sid/demo/DemoApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		if(1==1) {
+			throw new RuntimeException("I won't run today");
+		}
 		SpringApplication.run(DemoApplication.class, args);
-		throw  new RuntimeException("I won't run today");
 	}
 
 }
