a62a41f8c556f227529678081adcd98242da618c
siddharth Paul <siddharth.paul@zemosolabs.com>
2025-02-27 21:44:43 +0530

    fixed response

--- src/main/java/com/sid/demo/HomeController.java
+++ src/main/java/com/sid/demo/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -15,6 +16,7 @@ public class HomeController {
         return "index";
     }
     @GetMapping("/status")
+    @ResponseBody
     public String home() {
         return "ok";
     }
